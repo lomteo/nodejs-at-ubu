af5bf384bde70dde34f63bef5ef36f3da4d475b4
lomteo <31715141+lomteo@users.noreply.github.com>
2017-09-07 08:46:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<h1 style='color: red;'>Hello World!</h1>");
-    res.write("<h2 style='color: green;'>From</h2>");
+    res.write("<h1 style='color: green;'>Hello World!</h1>");
+    res.write("<h2 style='color: red;'>From</h2>");
     res.write("<h1 style='color: blue;'>Pedrung sriwarom 5611403082</h1>");
     res.end();
 }).listen(8080);
