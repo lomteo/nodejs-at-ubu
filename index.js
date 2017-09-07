093d8438b13e13e76ba2a8c277c8ca73d6550d93
lomteo <31715141+lomteo@users.noreply.github.com>
2017-09-07 10:52:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ http.createServer(function (req, res) {
     res.write("<h1 style='color: green;'>Hello World!</h1>");
     res.write("<h2 style='color: red;'>From</h2>");
     res.write("<h1 style='color: blue;'>Pedrung sriwarom 5611403082</h1>");
+    res.write("<h1 style='color: blue;'>alone</h1>");
     res.end();
 }).listen(8080);
